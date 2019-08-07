1455d2f26814195f5f0359ca5ea37ac099be5459
Mouni Mullapudi <mullapudi24@gmail.com>
2019-08-07 17:04:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ function destructivelyRemoveFirstDriver(name){
 function appendDriver (name) {
   return [...drivers, name];
 }
+function prependDriver (name) {
+  return [name, ...drivers];
+}
+
+function removeFirstDriver () {
+  return drivers.slice(1);
+}
+
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
